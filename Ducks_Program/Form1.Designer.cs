6ce62fd711245bd4400d17d8cbeca71bac6c621c
Rydzko <101673699+Rydzko@users.noreply.github.com>
2022-04-06 19:58:56 +0200

    Add files via upload
    It's a game of shooting ducks and scoring as many points as possible within the given time.

---
+++ Ducks_Program/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿
+namespace zad_dom_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox1.Location = new System.Drawing.Point(600, 73);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(188, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(600, 187);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 86);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.START_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label1.Location = new System.Drawing.Point(660, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "USERNAME";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Control;
+            this.button2.Location = new System.Drawing.Point(317, 318);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(160, 104);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "STOP";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.STOP_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(35, 367);
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.trackBar1.Size = new System.Drawing.Size(153, 45);
+            this.trackBar1.TabIndex = 4;
+            this.trackBar1.Value = 1;
+            this.trackBar1.Visible = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(606, 402);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(631, 378);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "SCORE";
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 428);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "EASY";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(150, 428);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "HARD";
+            this.label4.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(84, 428);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "MEDIUM";
+            this.label5.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Ducks_Program/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using WMPLib;
+
+namespace zad_dom_2
+{
+    public partial class Form1 : Form
+    {
+        WindowsMediaPlayer player = new WindowsMediaPlayer();
+        public Form1()
+        {
+            InitializeComponent();
+            kaczka.Click += new System.EventHandler(this.duck); //w tym momencie stara ilość kaczek nie zostanie dodana do nowej ilości kaczek i po złapaniu kaczki zostanie ona dodana raz, a nie kilka dwa, trzy itd
+            //(działo się tak, gdy EventHandler został wstawiony do START_Click)
+            //player.SoundLocation = "kaczuchy.mp3";
+            player.URL = "kaczuchy.mp3"; //dodanie muzyczki
+        }
+
+        Button kaczka = new Button(); //dynamiczne stworzenie kaczki
+        Random rand = new Random(); //będzie liczyć randomowe położenia kaczki
+
+        int licznik = 0; //bedzie dodawany jeśli trafie w kaczke
+        int wynik = 0; //będzie dodawany po każdym pojawieniu się kaczki
+
+        private void START_Click(object sender, EventArgs e) //operacje dla kaczki po kliknięciu buttona START
+        {
+            player.controls.play(); //play do muzyczki
+            MessageBox.Show("Ustrzel kaczuche i zdobądź jak największy wynik. Powodzenia !!!");
+            if (textBox1.Text != "")
+            {
+                label1.Visible = false; //Visible usówa obiekty, przyciski itd...
+                textBox1.Visible = false;
+                button1.Visible = false;
+
+                trackBar1.Visible = true;
+                button2.Visible = true;
+                textBox2.Visible = true;
+                label2.Visible = true;
+                label3.Visible = true;
+                label4.Visible = true;
+                label5.Visible = true;
+                kaczka.Visible = true; //SPRAWDZIĆ !!!
+
+                //kaczka.Click += new System.EventHandler(this.duck);
+                //kaczka.Text = "kaczucha";
+                Image kaczucha = Image.FromFile("kaczka.png"); //dodanie nowego tła gdy będziemy łapać kaczuchy(domyślne tło jest już ustawione na początku)
+                this.BackgroundImage = kaczucha;
+                ImageLayout ducker = ImageLayout.Stretch;
+                this.BackgroundImageLayout = ducker;
+                Image duck = Image.FromFile("duck.png"); //dodanie obrazu na kaczki
+                kaczka.BackgroundImage = duck;
+                ImageLayout kaczunia = ImageLayout.Stretch;
+                kaczka.BackgroundImageLayout = kaczunia;
+                Size size = new Size(50, 50);
+                kaczka.Size = size;
+
+                //Point location = kaczka.Location;
+                //location.X = rand.Next(500);
+                //location.Y = rand.Next(100);
+                //kaczka.Location = location;
+                textBox2.Text = wynik.ToString() + "/" + licznik.ToString(); //zapisanie wyniku w Scorze
+                kaczka.Location = new Point(rand.Next(0,500), rand.Next(0,100)); //nowa lokacja kaczki
+                Controls.Add(kaczka); //musimy dodać kaczke gdy się przemieści w inne miejsce
+                timer1.Enabled = true;
+                timer1.Interval = 2000;
+                //textBox2.Text = wynik.ToString() + "/" + "1";
+                //kaczka.Click += new System.EventHandler(this.timer1_Tick);
+            }
+            else
+            {
+                MessageBox.Show("Nie podałeś username. Uzupełnij TextBox");
+            }
+        }
+
+        private void duck(object sender, EventArgs e)
+        {
+            licznik++; //licznik liczący trafienia w kaczke
+            wynik++; //wynik, który będzie dodawał się za każdym razem, gdy kaczucha się przemieści
+            textBox2.Text = wynik.ToString() + "/" + licznik.ToString(); //zapisanie wyniku
+            //Point location = kaczka.Location;
+            //location.X = rand.Next(500);
+            //location.Y = rand.Next(100);
+            //kaczka.Location = location;
+            kaczka.Location = new Point(rand.Next(0, 500), rand.Next(0, 100));
+            Controls.Add(kaczka);
+        }
+
+        private void przemieszczenie()
+        {
+            //Point location = kaczka.Location;
+            //location.X = rand.Next(500);
+            //location.Y = rand.Next(100);
+            //kaczka.Location = location;
+            kaczka.Location = new Point(rand.Next(0, 500), rand.Next(0, 100));
+            textBox2.Text = wynik.ToString() + "/" + licznik.ToString();
+            Controls.Add(kaczka);
+        }
+
+        private void STOP_Click(object sender, EventArgs e)
+        {
+            using (StreamWriter saveScore = new StreamWriter("Wyniki.txt", true)) //zapisywanie do pliku
+            {
+                String sDate = DateTime.Now.ToString();
+                saveScore.WriteLine("USERNAME: " + textBox1.Text + "   " + "DATE: " + sDate + "   " + "SCORE: " + textBox2.Text); //zapisanie wyniku i bieżącej daty
+            }
+            label1.Visible = true; //Visible usówa obiekty, przyciski itd...
+            textBox1.Visible = true;
+            button1.Visible = true;
+            textBox1.Text = ""; //po naciśnięciu STOP wyzerowuje mi wpisaną nazwe użytkownika
+
+            trackBar1.Visible = false;
+            button2.Visible = false;
+            textBox2.Visible = false;
+            label2.Visible = false;
+            label3.Visible = false;
+            label4.Visible = false;
+            label5.Visible = false;
+            kaczka.Visible = false;
+            timer1.Enabled = false;
+            licznik = 0; //SPRAWDZIĆ !!!
+            wynik = 0; //musimy wyzerowac Score bo wyjściu z gry
+
+            Image kaczucha = Image.FromFile("kaczka_background.png"); //po naciśnięciu STOP nasz background zmieni się na ten sam, który był na początku gry
+            this.BackgroundImage = kaczucha;
+            ImageLayout ducker = ImageLayout.Stretch;
+            this.BackgroundImageLayout = ducker;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int time = 2000 / trackBar1.Value; //uzależniamy poziom trudności gry trackBarem1
+            double time_min = time * 0.8; //minimalny czas
+            double time_max = time * 1.2; //maksymalny czas
+            timer1.Interval = rand.Next((int)time_min, (int)time_max); //kiedy trafimy w kaczke, przemieści się ona w wyznaczonym przedziale czasu: (time_min, time_max)
+            licznik++;
+            przemieszczenie(); //wywołujemy samą funkcje przemieszczenie, ponieważ EventHandler dla kaczki wariował i przemieszczał sie w dziwny sposób
+            //timer1.Interval += rand.Next((int)(-timer1.Interval * 0.2), (int)(timer1.Interval * 0.2));
+            //timer1.Interval = 2000;
+            //licznik++;
+            //timer1.Tick += new System.EventHandler(this.przemieszczenie); //po uruchomieniu funkcji przemieszczenie kaczka wariowała
+        } 
+    }
+}
